61fcfea20b508353232c1215efd52193c66802fc
Evan Mullins <evanrm@gmail.com>
2022-06-29 12:19:56 -0400

    remove marketplace files and replace with module
    removes marketplace data components and images also version bump

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -2,4 +2,3 @@ export { default as Accordion } from './accordion';
 export { default as ErrorCard } from './errorCard';
 export { default as Header } from './header';
 export { default as Heading } from './heading';
-export { default as MarketplaceItem } from './marketplaceItem';
