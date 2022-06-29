d9fd76722888ba86d93071fa5e2da1e769bff5d3
Evan Mullins <evanrm@gmail.com>
2022-06-29 13:05:45 -0400

    add products fixture and update marketplace e2e test

--- tests/cypress/integration/marketplace.spec.js
+++ tests/cypress/integration/marketplace.spec.js
@@ -1,60 +1,130 @@
 // <reference types="Cypress" />
+const marketplace = require('../fixtures/products.json');
 
 describe('Marketplace Page', function () {
 
 	before(() => {
+		cy.server();
+		cy.route('GET', '**/newfold-marketplace/v1/marketplace**', 'fx:marketplace').as('marketplace');
 		cy.visit('/wp-admin/admin.php?page=web#/marketplace');
 		cy.injectAxe();
 	});
 
+	it('Exists', () => {
+		cy.contains('button', 'Everything');
+	});
+
 	it('Is Accessible', () => {
-		cy.wait(500);
+		cy.wait(1000);
 		cy.checkA11y('.wppw-app-body');
 	});
 
-
-	it('Plugins tab is default/first', () => {
-		cy.get('.components-tab-panel__tabs-item.plugins').should('have.class', 'current-tab');
-		cy.get('.components-tab-panel__tabs-item.services').should('not.have.class', 'current-tab');
-		cy.get('.components-tab-panel__tabs-item.themes').should('not.have.class', 'current-tab');
-		cy
-			.get('.components-tab-panel__tab-content')
-			.contains('h3', 'Plugins')
-			.should('have.class', 'screen-reader-text');
+	it('Product grid has 12 items', () => {
+		cy.get('.marketplace-item').should('have.length', 12);
 	});
 
-	it('Plugins tab contains Yoast Premium', () => {
-		cy
-			.get('.marketplace-item-yoast-premium')
-			.contains('h4', 'Yoast SEO Premium');
-		cy
-			.get('.marketplace-item-yoast-premium')
-			.contains('a', 'Buy Now')
+	it('First product card renders correctly', () => {
+		cy.get('#marketplace-item-1fc92f8a-bb9f-47c8-9808-aab9c82d6bf2').as('card');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Learn More'})
+			.scrollIntoView()
+			.should('be.visible')
 			.should('have.attr', 'href')
-			.and('match', /yoa.st/);
+			.and('include', 'https://www.web.com/websites/website-design-services');
+
+		cy.get('@card').first().within(() => {
+			cy.get('.components-card__header')
+				.contains('Web Design Services')
+				.should('be.visible');
+			cy.get('.components-card__media').should('be.visible');
+			cy.get('.components-card__header em.price').should('not.exist');
+		});
 	});
 
-	it('Plugins tab contains Yoast Woo', () => {
-		cy
-			.get('.marketplace-item-yoast-woo')
-			.contains('h4', 'Yoast WooCommerce SEO');
-		cy
-			.get('.marketplace-item-yoast-woo')
-			.contains('a', 'Buy Now')
+	it('Second product card render correctly', () => {
+		cy.get('#marketplace-item-2a1dadb5-f58d-4ae4-a26b-27efb09136eb').as('card');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Buy Now'})
+			.scrollIntoView()
+			.should('be.visible')
 			.should('have.attr', 'href')
-			.and('match', /yoa.st/);
+			.and('include', 'https://www.mojomarketplace.com/cart?item_id=5377b431-d8a8-431b-a711-50c10a141528');
+
+		cy.get('@card').first().within(() => {
+			cy.get('.components-card__header')
+				.contains('Highend')
+				.should('be.visible');
+			cy.get('.components-card__media').should('be.visible');
+			cy.get('.components-card__header em.price')
+				.contains('$59.00')
+				.should('be.visible');
+		});
+	});
+	
+	it('CTA links have target=_blank', () => {
+		cy.get('#marketplace-item-1fc92f8a-bb9f-47c8-9808-aab9c82d6bf2').as('card');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Learn More'})
+			.scrollIntoView()
+			.should('have.attr', 'target')
+			.and('include', '_blank');
 	});
 
-	it('Tabs switch to proper content', () => {
-		cy.get('.components-tab-panel__tabs-item.services').click();
-		cy.wait(500);
-		cy.hash().should('eq', '#/marketplace/services');
-		cy.get('.components-tab-panel__tabs-item.services').should('have.class', 'current-tab');
+	it('Load more button loads more products', () => {
+		cy.get('.marketplace-item').should('have.length', 12);
 
-		cy
-			.get('.components-tab-panel__tab-content')
-			.contains('h3', 'Services')
-			.should('have.class', 'screen-reader-text');
+		cy.contains('button', 'Load More');
+
+		cy.get('.marketplaceList button')
+			.scrollIntoView()
+			.click();
+
+		cy.wait(300);
+
+		cy.get('.marketplace-item').should('have.length', 19);
 	});
+
+	it('Category Tab Filters properly', () => {
+		cy.contains('button', 'Services');
+		cy.contains('button', 'SEO');
+
+		cy.get('#tab-panel-0-Services').click();
+		cy.wait(300);
+		cy.get('.marketplace-item').should('have.length', 5);
+
+		cy.get('#marketplace-item-1fc92f8a-bb9f-47c8-9808-aab9c82d6bf2 h3')
+			.scrollIntoView()
+			.should('be.visible')
+			.should('have.text', 'Web Design Services');
+		
+		cy.get('#tab-panel-0-SEO').click();
+		cy.wait(300);
+		cy.get('.marketplace-item').should('have.length', 3);
+	
+		cy.get('#marketplace-item-7beee5ae-2e91-4282-9930-15ada43fc738 h3')
+			.scrollIntoView()
+			.should('be.visible')
+			.should('have.text', 'Yoast Premium');
+	});
+
+	// CTB Not supported yet
+	// it('Product CTB cards render correctly', () => {
+	// 	cy.get('.marketplace-item-ec14a614-8672-4094-8310-cb0b1eb0f176').as('card');
+
+	// 	cy.get('@card')
+	// 		.findByRole('button', {name: 'Buy Now'})
+	// 		.scrollIntoView()
+	// 		.should('be.visible')
+	// 		.should('have.attr', 'data-action')
+	// 		.and('include', 'load-nfd-ctb');
+
+	// 	cy.get('@card').first().within(() => {
+	// 		cy.get('.components-card__header').should('be.visible');
+	// 		cy.get('.components-card__media').should('be.visible');
+	// 	});
+	// });
 
 });
