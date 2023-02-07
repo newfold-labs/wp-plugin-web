82ecbdd785794753cd8d20ab38a990cb34067427
Evan Mullins <evanrm@gmail.com>
2023-02-07 15:14:18 -0500

    update tested up to version

--- wp-plugin-web.php
+++ wp-plugin-web.php
@@ -14,7 +14,7 @@
  * Version:           1.2.4
  * Requires at least: 4.7
  * Requires PHP:      5.6
- * Tested up to:      5.9
+ * Tested up to:      6.1.1
  * Author:            Web.com
  * Author URI:        https://web.com
  * Text Domain:       wp-plugin-web
