8206921807bc079a99aaa980a1b9c548c5d5c277
Evan Mullins <evanrm@gmail.com>
2024-12-19 12:36:48 -0500

    bump wordpress version

--- wp-plugin-web.php
+++ wp-plugin-web.php
@@ -15,7 +15,7 @@
  * Version:           2.1.5
  * Requires at least: 6.5
  * Requires PHP:      7.1
- * Tested up to:      6.7
+ * Tested up to:      6.7.1
  * Author:            Web.com
  * Author URI:        https://web.com
  * Text Domain:       wp-plugin-web
