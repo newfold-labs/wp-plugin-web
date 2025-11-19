3c573e149429712d8a75049b9206ca14f5d02279
Ramya krishna <ramyakrishnaimminni@gmail.com>
2025-11-19 17:41:28 +0530

    class chnages

--- tests/cypress/integration/settings.cy.js
+++ tests/cypress/integration/settings.cy.js
@@ -126,8 +126,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(100);
 		cy.get('[data-id="content-revisions-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:first')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:first')
 			.click(); // 1
 		cy.wait(100);
 		cy.get('#content-revisions-select__description')
@@ -138,8 +138,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(1000);
 		cy.get('[data-id="content-revisions-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:last')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:last')
 			.click(); // 40
 		cy.wait(100);
 		cy.get('#content-revisions-select__description')
@@ -150,8 +150,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(500);
 		cy.get('[data-id="content-revisions-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:nth-child(2)')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:nth-child(2)')
 			.click(); // 5
 		cy.get('#content-revisions-select__description')
 			.contains('you can take 5 steps back.')
@@ -162,8 +162,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(500);
 		cy.get('[data-id="empty-trash-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:nth-child(2)')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:nth-child(2)')
 			.click(); // 2
 		cy.wait(100);
 		cy.get('#empty-trash-select__description')
@@ -174,8 +174,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(500);
 		cy.get('[data-id="empty-trash-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:last')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:last')
 			.click(); // 4
 		cy.wait(100);
 		cy.get('#empty-trash-select__description')
@@ -192,8 +192,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(500);
 		cy.get('[data-id="comments-per-page-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:first')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:first')
 			.click(); // 10
 		cy.wait(100);
 		cy.get( appClass + '-app-settings-comments')
@@ -213,8 +213,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(100);
 		cy.get('[data-id="close-comments-days-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:last')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:last')
 			.click(); // 100
 		cy.wait(100);
 		cy.get( appClass + '-app-settings-comments')
@@ -225,8 +225,8 @@ describe('Settings Page', { testIsolation: true }, () => {
 		cy.wait(100);
 		cy.get('[data-id="close-comments-days-select"]')
 			.parent()
-			.next('ul.nfd-select__options')
-			.find('li:nth-child(6)')
+			.next('.nfd-select__options')
+			.find('.nfd-select__option:nth-child(6)')
 			.click(); // 14
 		cy.wait(100);
 		cy.get( appClass + '-app-settings-comments')
