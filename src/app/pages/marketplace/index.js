c938367c39989d4b5c0cc563dd3d642e9d2bca5d
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2025-12-02 01:01:21 +0530

    updating the marketplace module to 3.1.2

--- src/app/pages/marketplace/index.js
+++ src/app/pages/marketplace/index.js
@@ -3,16 +3,18 @@ import { useState, useEffect } from '@wordpress/element';
 import { useLocation, useMatch, useNavigate } from 'react-router-dom';
 import { Container, Page } from '@newfold/ui-component-library';
 import { NewfoldRuntime } from "@newfold/wp-module-runtime";
-import { default as NewfoldMarketplace } from '@modules/wp-module-marketplace/components/';
+import { default as MarketplaceHeader } from '@modules/wp-module-marketplace/components/marketplaceHeader';
+import { default as MarketplaceBody } from '@modules/wp-module-marketplace/components/marketplaceBody';
 
 const MarketplacePage = () => {
 	
     // constants to pass to module
 	const moduleConstants = {
 		'supportsCTB': false,
+		'perPage': 12,
 		'text': {
 			title: __('Marketplace', 'wp-plugin-web'),
-			subTitle: __('Explore our featured collection of tools and services.', 'wp-plugin-web'),
+			subTitle: __('Explore our marketplace plugins and services.', 'wp-plugin-web'),
 			error: __('Oops, there was an error loading the marketplace, please try again later.', 'wp-plugin-web'),
 			noProducts: __('Sorry, no marketplace items. Please, try again later.', 'wp-plugin-web'),
 			loadMore: __('Load More', 'wp-plugin-web'),
@@ -45,8 +47,13 @@ const MarketplacePage = () => {
 	return (
         <Page className={"wppw-app-marketplace-page marketplace-with-icon"}>
 			<Container className={'wppw-app-marketplace-container nfd-overflow-clip'}>
+				
+				<MarketplaceHeader 
+					title={moduleConstants.text.title}
+					description={moduleConstants.text.subTitle}
+				/>
 
-				<NewfoldMarketplace 
+				<MarketplaceBody 
 					methods={moduleMethods}
 					constants={moduleConstants}
 				/>
