bc97e48aa9969a7b3bca3f79e412d8c17a63f913
Ajay Yadav <ajayadav09@gmail.com>
2025-12-11 18:35:56 +0530

    site preview

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -1,4 +1,5 @@
 
+import { useEffect } from '@wordpress/element';
 import { Page, Container, Title } from "@newfold/ui-component-library";
 import { HomeIcon } from '@heroicons/react/24/outline';
 import ComingSoon from 'App/pages/settings/comingSoon';
@@ -7,8 +8,35 @@ import WebContentSection from 'App/pages/home/webContentSection';
 import WebHostingSection from 'App/pages/home/webHostingSection';
 
 const Home = () => {
+	const siteKind = window.NewfoldRuntime.siteType || 'website';
+
+	useEffect( () => {
+		// run when mounts
+		const comingSoonPortal =
+			document.getElementById( 'coming-soon-portal' );
+
+
+		if ( comingSoonPortal ) {
+			window.NFDPortalRegistry.registerPortal(
+				'coming-soon',
+				comingSoonPortal
+			);
+		}
+
+		// run when unmounts
+		return () => {
+			window.NFDPortalRegistry.unregisterPortal( 'coming-soon' );
+		};
+	}, [] );
+
 	return (
 	<Page title="Home" className={"wppw-app-home-page wppw-home"}>
+		{ siteKind !== 'store' && (
+			// Coming soon portal for blog and corporate sites only
+			<Container className="nfd-max-w-full nfd-p-8 nfd-shadow-none nfd-rounded-xl nfd-border nfd-border-[#D5D5D5]">
+				<div id="coming-soon-portal" />
+			</Container>
+		) }
 		<Container className={'wppw-app-home-container'}>
 			<Container.Header className={'wppw-app-home-header'}>
 				<Title as="h2" className="nfd-flex nfd-items-center nfd-gap-2">
