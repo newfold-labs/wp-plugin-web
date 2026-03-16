7d36431c1aa4f11026fed29b83f35d99208b59ff
Ramya krishna <ramyakrishnaimminni@gmail.com>
2026-03-16 11:49:11 +0530

    added z-index to notifications , refactored store details code, upgraded ecommerce module version to  accomodate the web plugin code changes

--- src/app/pages/home/storeDetails.js
+++ src/app/pages/home/storeDetails.js
@@ -3,30 +3,31 @@ import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { PartyIcon } from '../../components/icons';
 
-const StoreDetails = () => {
-    const [ hasStoreInfo, setHasStoreInfo ] = useState(
-		!! (
-			window?.NFDStoreInfo?.data?.address &&
-			window?.NFDStoreInfo?.data?.city
-		)
+const hasRequiredStoreInfo = () =>
+	Boolean(
+		window?.NFDStoreInfo?.data?.address &&
+		window?.NFDStoreInfo?.data?.city
 	);
 
-    const siteKind = window.NewfoldRuntime.siteType || 'website';
+const StoreDetails = () => {
+    const [ hasStoreInfo, setHasStoreInfo ] = useState( hasRequiredStoreInfo );
+    const siteKind = window?.NewfoldRuntime?.siteType || 'website';
+    const titleText =
+		siteKind === 'store'
+			? __( 'Congrats, your store is live!', 'wp-plugin-web' )
+			: __( 'Congrats, your website is live!', 'wp-plugin-web' );
+    const detailsButtonText = hasStoreInfo
+		? __( 'Store Details', 'wp-plugin-web' )
+		: __( 'Add Store Details', 'wp-plugin-web' );
 
 	useEffect( () => {
-		// Update hasStoreInfo when storeInfo changes
-		const handleStoreInfoChange = () => {
-			setHasStoreInfo(
-				!! (
-					window?.NFDStoreInfo?.data?.address &&
-					window?.NFDStoreInfo?.data?.city
-				)
-			);
-		};
+		const handleStoreInfoChange = () => setHasStoreInfo( hasRequiredStoreInfo() );
+
 		document.addEventListener(
 			'nfd-submit-store-info-form',
 			handleStoreInfoChange
 		);
+
 		return () => {
 			document.removeEventListener(
 				'nfd-submit-store-info-form',
@@ -35,39 +36,25 @@ const StoreDetails = () => {
 		};
 	}, [] );
 
-    return <div className="nfd-home__title-section nfd-flex nfd-justify-between nfd-items-center">
-				<span
-					className={
-						'nfd-home__title-wrapper nfd-flex nfd-gap-4 nfd-items-center'
-					}
+    return (
+		<div className="nfd-home__title-section nfd-flex nfd-justify-between nfd-items-center">
+			<span className="nfd-home__title-wrapper nfd-flex nfd-gap-4 nfd-items-center">
+				<PartyIcon />
+				<Title className="nfd-mb-1 nfd-font-semibold">{ titleText }</Title>
+			</span>
+
+			{ siteKind === 'store' && (
+				<Button
+					as={ 'a' }
+					href={ '#' }
+					data-store-info-trigger
+					variant={ 'secondary' }
 				>
-					<PartyIcon />
-					<Title className="nfd-mb-1 nfd-font-semibold">
-						{ siteKind === 'store'
-							? __(
-									'Congrats, your store is live!',
-									'wp-plugin-web'
-							  )
-							: __(
-									'Congrats, your website is live!',
-									'wp-plugin-web'
-							  ) }
-					</Title>
-				</span>
-				{ siteKind === 'store' && (
-					// Store details button only on store sites
-					<Button
-						as={ 'a' }
-						href={ '#' }
-						data-store-info-trigger
-						variant={ 'secondary' }
-					>
-						{ hasStoreInfo
-							? __( 'Store Details', 'wp-plugin-web' )
-							: __( 'Add Store Details', 'wp-plugin-web' ) }
-					</Button>
-				) }
-			</div>
-}
+					{ detailsButtonText }
+				</Button>
+			) }
+		</div>
+	);
+};
 
 export default StoreDetails;
